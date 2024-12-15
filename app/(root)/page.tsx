82eaae9ddee7241ab4878e40374841b769e56d6d
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-12-15 19:28:13 +0000

    Create startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
-import SearchForm from "../../components/ui/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Costa" },
+      _id: 1,
+      description: "this is a description.",
+      image: "https://i.insider.com/67093b013f2165d716e02b52?width=700",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -20,6 +34,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={"startup_card_" + index} post={post} />
+            ))
+          ) : (
+            <p className="no-results"> No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
       colors: {
         primary: {
           "100": "#FFE8F0",
-          DEFAULT: "#EE2B69",
+          DEFAULT: "#EE2B60",
         },
         secondary: "#FBE843",
         black: {

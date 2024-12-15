39bde35d32e529625e1267aedee1e72ae683fc2c
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-12-15 17:56:49 +0000

    Complete server side form component and add lucide-react icons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -13,7 +19,7 @@ export default function Home() {
           Competitions.
         </p>
 
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
     </>
   );

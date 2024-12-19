0f5b49a7432a1f76a3ca7f60f06a1c37d51bea94
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-12-19 21:31:57 +0000

    create sanity query and replace the hardcoded posts with the fetched ones

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -8,18 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "Costa" },
-      _id: 1,
-      description: "this is a description.",
-      image: "https://i.insider.com/67093b013f2165d716e02b52?width=700",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const posts = await client.fetch(STARTUP_QUERY);
 
   return (
     <>
